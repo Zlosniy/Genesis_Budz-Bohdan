eb0d9f9c329581da57aa0ce308904d11bb318bee
NataliyaMylyan <mnatal2015@gmail.com>
2021-11-17 21:56:04 +0200

    updated

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,30 +1,24 @@
 const galleryRef = document.querySelector('.gallery');
 
-  async function getUserInfo() {
-    const options = {
-      method: 'GET',
+async function getUserInfo() {
+  const options = {
+    method: 'GET',
     //   params: { video_url: 'https://vm.tiktok.com/ZMeb45Cv2/' },
-      // url: 'https://tiktok33.p.rapidapi.com/trending/feed',
-      headers: {
-        'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
-        'x-rapidapi-key': 'c4915b95f9msh933e1431e58ddb9p1ed21ajsn1d68718f52f9',
-      },
-    };
-  
-    return await fetch('https://tiktok33.p.rapidapi.com/user/info/dave.xp', options)
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-        // const markup = card(data);
-        // console.log(markup);
-        // galleryRef.insertAdjacentHTML('beforeend', markup);
-        // const imageRef = document.querySelector('.avatar-image');
-        // console.log(imageRef);
-        // imageRef.addEventListener('click', showProfile);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+    // url: 'https://tiktok33.p.rapidapi.com/trending/feed',
+    headers: {
+      'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
+      'x-rapidapi-key': 'c4915b95f9msh933e1431e58ddb9p1ed21ajsn1d68718f52f9',
+    },
+  };
+
+  return await fetch('https://tiktok33.p.rapidapi.com/user/info/dave.xp', options)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
